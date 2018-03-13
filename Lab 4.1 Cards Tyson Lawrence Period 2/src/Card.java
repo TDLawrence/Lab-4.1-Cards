578a9be762e08ad5bda0685bbd4fef744977a18b
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-03-13 09:21:05 -0400

    Added getters for card class and created Deck constructor.

--- Lab 4.1 Cards Tyson Lawrence Period 2/src/Card.java
+++ Lab 4.1 Cards Tyson Lawrence Period 2/src/Card.java
@@ -5,6 +5,18 @@ public class Card {
 	private String suit;
 	private int value;
 	
+	public String getRank() {
+		return rank;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
 	public Card(String rank, String suit,int value)
 	{this.rank=rank;
 	 this.suit=suit;
--- Lab 4.1 Cards Tyson Lawrence Period 2/src/Deck.java
+++ Lab 4.1 Cards Tyson Lawrence Period 2/src/Deck.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
 
 public class Deck {
 
+	
+private String[] ranks;
+private String[]suits;
+private int[]values;
+ArrayList<Card> Undealt;
+
+public Deck(String[}ranks,String[]suits,int[]values)
+{for (int x=0;x<ranks.length;x++)
+ {for (int y=0;y<suits.length;y++)
+  {
+	 Card card=new Card(ranks[x],suits[y],values[x]);
+	 Undealt.add(card);
+  }
+	
+	
+ }
+}
 }
